d84a28600b693914a8771331dcc480a440bb887e
root <root@lanjie.bcc-szwg.baidu.com>
2021-02-09 14:02:14 +0800

    [update]deploy script

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,1 +1,4 @@
-kind create cluster --name lanjie --config config.yaml
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
+
+kind create cluster --name lanjie --config ${dir_path}/kindClusterConfig/config.yaml
