ad287a3b8b4d877db86795c23366496c9f3c0af6
root <root@lanjie.bcc-szwg.baidu.com>
2021-02-22 09:22:18 +0800

    update

--- install_elk.sh
+++ install_elk.sh
@@ -1,5 +1,5 @@
 # ref: https://github.com/elastic/helm-charts/tree/7.7.1/elasticsearch/
 helm repo add elastic https://helm.elastic.co
-helm install --name elasticsearch --version 7.7.1 elastic/elasticsearch --namespace monitoring
-helm install --name logstash --version 7.7.1 elastic/logstash --namespace monitoring
-helm install --name kibana --version 7.7.1 elastic/kibana --namespace monitoring
+helm install elasticsearch --version 7.7.1 elastic/elasticsearch --namespace monitoring
+helm install logstash --version 7.7.1 elastic/logstash --namespace monitoring
+helm install kibana --version 7.7.1 elastic/kibana --namespace monitoring
--- install_ingress_controller.sh
+++ install_ingress_controller.sh
@@ -1,3 +1,4 @@
+##################方式一##################
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
 helm fetch ingress-nginx/ingress-nginx --version 3.19.0
@@ -6,7 +7,32 @@ docker pull yametech/ingress-controller:v0.43.0
 docker tag docker.io/yametech/ingress-controller:v0.43.0 k8s.gcr.io/ingress-nginx/controller:v0.43.0
 kind load docker-image k8s.gcr.io/ingress-nginx/controller:v0.43.0 --name lanjikind
 
-# 修改一下镜像信息,把digest信息注释掉.
+# 修改values.yaml文件
+# 1. 修改 controller.hostNetwork: true
+# 2. 修改 controller.hostPort.enabled: true
+# 3. 删除 controller.service.type: LoadBalancer
+# 4. 添加 controller.service.type: NodePort
+#         controller.service.nodePorts.http: 32080
+#         controller.service.nodePorts.https: 32443
+
 # 进入 chart 目录进行安装
 helm install ingress-nginx -f values.yaml  --namespace=ingress-nginx .
 
+# 访问服务
+# 参考包中的NOTES.txt
+
+# 获取 svc ingress-nginx-controller 中的 NodePort (根据前面对values.yaml文件的修改可知，NodPort的值为32080/32443)
+curl http://{NODE_IP}:{NodePort}/bar
+
+
+##################方式二#################
+
+# 参考以下链接
+# https://raw.githubusercontent.com/kubernetes/ingress-nginx/helm-chart-3.20.0/deploy/static/provider/kind/deploy.yaml
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
+
+kubectl apply -f ${dir_path}/examples/ingress/deploy.yaml
+
+# 如何访问服务
+curl localhost/bar
